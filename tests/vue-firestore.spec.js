83b2dc5365615b6dafb310c6e095f460c9591c2e
Amrani Houssain <amranidev@gmail.com>
2017-11-01 00:02:48 +0000

    update tests

--- tests/vue-firestore.spec.js
+++ tests/vue-firestore.spec.js
@@ -2,17 +2,30 @@ import Vue from "vue"
 
 import VueFirestore from "./../dist/vue-firestore"
 
+import Firebase from "firebase"
+
+var config = {
+    apiKey: "AIzaSyB9Trlbrpo48ilkNHZ6MGbofFf2u8uHuRA",
+    authDomain: "oss-test-myfirebase.firebaseapp.com",
+    databaseURL: "https://oss-test-myfirebase.firebaseio.com",
+    projectId: "oss-test-myfirebase",
+};
+
 Vue.use(VueFirestore)
+
+import "firebase/firestore"
+
+var firebase = Firebase.initializeApp(config);
 
 describe('vue-firestore', function() {
     var fireStore
     beforeEach(function(done) {
-
+        fireStore = firebase.firestore()
         done()
     })
 
     describe('Check Function options', function() {
-        it('Option is callable as function?', function() {
+        it('Option is callable as function?', function(done) {
             var spy = sinon.spy()
             expect(function() {
                 new Vue({
@@ -20,6 +33,29 @@ describe('vue-firestore', function() {
                 }).$mount()
             }).to.not.throw()
             expect(spy.calledOnce).to.be.true
+            done()
+        })
+    })
+    describe('Add data to firebase database', function() {
+        it("will add data to cloud firestore", function(done) {
+            var vm = new Vue({
+                firestore: function() {
+                    return {
+                        items: fireStore.collection('items')
+                    }
+                }
+            }).$mount()
+            fireStore.collection('items').doc("mybook").set({
+                title: "MyBook 1",
+                author: "My Author"
+            }).then(function() {
+                expect(vm.items).to.deep.equal([
+                    { ".key": "mybook", title: "MyBook 1", author: "My Author" }
+                ])
+            }).then(done, done)
         })
     })
 })
+
+/*
+ */
