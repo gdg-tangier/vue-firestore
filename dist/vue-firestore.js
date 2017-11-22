0c968e75036a7560d46c585025f61e39e60aba51
Amrani Houssain <amranidev@gmail.com>
2017-11-22 18:07:50 +0000

    build

--- dist/vue-firestore.js
+++ dist/vue-firestore.js
@@ -1,4 +1,14 @@
-define("Vue-firestore", [], function() { return /******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["Vue-firestore"] = factory();
+	else
+		root["Vue-firestore"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -237,4 +247,5 @@ function ensureRefs(vm) {
 }
 
 /***/ })
-/******/ ])});;
+/******/ ]);
+});
