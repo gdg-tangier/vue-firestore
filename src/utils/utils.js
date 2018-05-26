14063f377f5cf9b70b11cd7dc51b6832b86e60ee
Amrani Houssain <houssain@airbit.com>
2018-05-26 17:16:43 +0100

    formatting

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,33 +1,33 @@
 /**
  * Check if a record is an object.
- * 
- * @param {*} val 
+ *
+ * @param {*} val
  * @return {boolean}
  */
-export function isObject(val) {
-    return Object.prototype.toString.call(val) === '[object Object]'
+export function isObject (val) {
+  return Object.prototype.toString.call(val) === '[object Object]'
 }
 
 /**
  * Normalize Firebase snapshot into a bindable data record.
- * 
+ *
  * @param {FirebaseSnapshot} snapshot
  * @return {object}
  */
-export function normalize(snapshot) {
-    var value = snapshot.doc ? snapshot.doc.data() : snapshot.data()
-    var out = isObject(value) ? value : { '.value': value }
-    out['.key'] = snapshot.doc ? snapshot.doc.id : snapshot.id
-    return out;
+export function normalize (snapshot) {
+  var value = snapshot.doc ? snapshot.doc.data() : snapshot.data()
+  var out = isObject(value) ? value : { '.value': value }
+  out['.key'] = snapshot.doc ? snapshot.doc.id : snapshot.id
+  return out
 }
 
 /**
  * Ensure firebasestore option on a vue instance.
- * 
- * @param {Vue} vm 
+ *
+ * @param {Vue} vm
  */
-export function ensureRefs(vm) {
-    if (!vm.$firestore) {
-        vm.$firestore = Object.create(null)
-    }
+export function ensureRefs (vm) {
+  if (!vm.$firestore) {
+    vm.$firestore = Object.create(null)
+  }
 }
