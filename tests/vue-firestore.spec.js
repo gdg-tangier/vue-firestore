6e97c8007a332dc8f7a9c3d76d59e46be7864458
Amrani Houssain <amranidev@gmail.com>
2018-12-11 22:55:37 +0000

    Setup test env

--- tests/vue-firestore.spec.js
+++ tests/vue-firestore.spec.js
@@ -5,10 +5,12 @@ import VueFirestore from './../dist/vue-firestore'
 import Firebase from 'firebase'
 
 var config = {
-    apiKey: 'AIzaSyB9Trlbrpo48ilkNHZ6MGbofFf2u8uHuRA',
-    authDomain: 'oss-test-myfirebase.firebaseapp.com',
-    databaseURL: 'https://oss-test-myfirebase.firebaseio.com',
-    projectId: 'oss-test-myfirebase'
+    apiKey: "AIzaSyB9Trlbrpo48ilkNHZ6MGbofFf2u8uHuRA",
+    authDomain: "oss-test-myfirebase.firebaseapp.com",
+    databaseURL: "https://oss-test-myfirebase.firebaseio.com",
+    projectId: "oss-test-myfirebase",
+    storageBucket: "oss-test-myfirebase.appspot.com",
+    messagingSenderId: "10529373536"
 }
 
 Vue.use(VueFirestore)
@@ -16,55 +18,39 @@ Vue.use(VueFirestore)
 import 'firebase/firestore'
 
 var firebase = Firebase.initializeApp(config)
+var firestore = firebase.firestore()
 
-describe('vue-firestore', function() {
-    var fireStore
-    beforeEach(function(done) {
-        fireStore = firebase.firestore()
-        fireStore.settings({ timestampsInSnapshots: true })
-        var items = fireStore.collection('items').get()
+firestore.settings({timestampsInSnapshots: true})
 
-        //delete items collection before each tests
-        items.then(function(snapshot) {
-            snapshot.forEach(function(doc) {
-                items.doc(doc.id).delete()
-            })
-        })
+function VueTick() {
+  return new Promise((resolve, reject) => {
+    Vue.nextTick(resolve)
+  })
+}
 
-        done()
+let vm, collection
+describe('vue-firestore', () => {
+    beforeEach( async (done) => {
+      collection = firestore.collection('items')
+      // var doc = collection.doc()
+      vm = new Vue({
+        data: () => ({
+          items: null,
+          item: null
+        }),
+        firestore () {
+          return {
+            items: collection,
+            // item: doc
+          }
+        }
+      })
+      await VueTick()
+      done()
     })
 
-    describe('Check Function options', function() {
-        it('Option is callable as function?', function(done) {
-            var spy = sinon.spy()
-            expect(function() {
-                new Vue({
-                    firestore: spy
-                }).$mount()
-            }).to.not.throw()
-            expect(spy.calledOnce).to.be.true
-            done()
-        })
+    test('setups $firestore', () => {
+      expect(Object.keys(vm.$firestore).sort()).toEqual(['items'])
+      expect(vm.$firestore.items).toBe(collection)
     })
-        /*
-        describe('Add data to firebase database', function() {
-            it('will add data to cloud firestore', function(done) {
-                var vm = new Vue({
-                    firestore: function() {
-                        return {
-                            items: fireStore.collection('items')
-                        }
-                    }
-                }).$mount()
-                fireStore.collection('items').doc('mybook').set({
-                    title: 'MyBook 1',
-                    author: 'My Author'
-                }).then(function() {
-                    expect(vm.items).to.deep.equal([
-                        { '.key': 'mybook', title: 'MyBook 1', author: 'My Author' }
-                    ])
-                }).then(done, done)
-            })
-        })
-        */
 })
