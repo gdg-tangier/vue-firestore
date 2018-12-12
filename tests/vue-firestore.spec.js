b8d0695406badc902a6666cef4a5b419efa4ab13
Amrani Houssain <amranidev@gmail.com>
2018-12-12 22:48:57 +0000

    update tests

--- tests/vue-firestore.spec.js
+++ tests/vue-firestore.spec.js
@@ -1,56 +1,30 @@
-import Vue from 'vue'
-
-import VueFirestore from './../dist/vue-firestore'
-
-import Firebase from 'firebase'
-
-var config = {
-    apiKey: "AIzaSyB9Trlbrpo48ilkNHZ6MGbofFf2u8uHuRA",
-    authDomain: "oss-test-myfirebase.firebaseapp.com",
-    databaseURL: "https://oss-test-myfirebase.firebaseio.com",
-    projectId: "oss-test-myfirebase",
-    storageBucket: "oss-test-myfirebase.appspot.com",
-    messagingSenderId: "10529373536"
-}
-
-Vue.use(VueFirestore)
-
-import 'firebase/firestore'
-
-var firebase = Firebase.initializeApp(config)
-var firestore = firebase.firestore()
-
-firestore.settings({timestampsInSnapshots: true})
-
-function VueTick() {
-  return new Promise((resolve, reject) => {
-    Vue.nextTick(resolve)
-  })
-}
+import { Vue, firebase, firestore, VueTick } from './TestCase'
 
 let vm, collection
+
 describe('vue-firestore', () => {
-    beforeEach( async (done) => {
+    beforeEach( async () => {
       collection = firestore.collection('items')
-      // var doc = collection.doc()
       vm = new Vue({
         data: () => ({
           items: null,
-          item: null
         }),
         firestore () {
           return {
             items: collection,
-            // item: doc
           }
         }
       })
       await VueTick()
-      done()
     })
 
-    test('setups $firestore', () => {
+    test('setup $firestore', () => {
       expect(Object.keys(vm.$firestore).sort()).toEqual(['items'])
       expect(vm.$firestore.items).toBe(collection)
+    })
+
+    test('unbind $firestore on $destroy', () => {
+      vm.$destroy()
+      expect(vm.$firestore).toEqual(null)
     })
 })
