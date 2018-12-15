95d15b7952b82558c12059fb380954ce54cdb9cc
Amrani Houssain <amranidev@gmail.com>
2018-12-15 12:37:39 +0000

    update vue-firestore tests

--- tests/vue-firestore.spec.js
+++ tests/vue-firestore.spec.js
@@ -3,28 +3,28 @@ import { Vue, firebase, firestore, VueTick, randomString } from './TestCase'
 let vm, collection
 
 describe('vue-firestore', () => {
-    beforeEach( async () => {
-      collection = firestore.collection('items')
-      vm = new Vue({
-        data: () => ({
-          items: null,
-        }),
-        firestore () {
-          return {
-            items: collection,
-          }
+  beforeEach(async () => {
+    collection = firestore.collection('items')
+    vm = new Vue({
+      data: () => ({
+        items: null
+      }),
+      firestore () {
+        return {
+          items: collection
         }
-      })
-      await VueTick()
+      }
     })
+    await VueTick()
+  })
 
-    test('setup $firestore', () => {
-      expect(Object.keys(vm.$firestore).sort()).toEqual(['items'])
-      expect(vm.$firestore.items).toBe(collection)
-    })
+  test('setup $firestore', () => {
+    expect(Object.keys(vm.$firestore).sort()).toEqual(['items'])
+    expect(vm.$firestore.items).toBe(collection)
+  })
 
-    test('unbind $firestore on $destroy', () => {
-      vm.$destroy()
-      expect(vm.$firestore).toEqual(null)
-    })
+  test('unbind $firestore on $destroy', () => {
+    vm.$destroy()
+    expect(vm.$firestore).toEqual(null)
+  })
 })
