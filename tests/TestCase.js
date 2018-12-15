93020f22984c98024445095951e04163da4f8769
Amrani Houssain <amranidev@gmail.com>
2018-12-15 12:37:27 +0000

    update TestCase

--- tests/TestCase.js
+++ tests/TestCase.js
@@ -5,12 +5,12 @@ import VueFirestore from './../dist/vue-firestore'
 import Firebase from 'firebase'
 
 var config = {
-    apiKey: "AIzaSyB9Trlbrpo48ilkNHZ6MGbofFf2u8uHuRA",
-    authDomain: "oss-test-myfirebase.firebaseapp.com",
-    databaseURL: "https://oss-test-myfirebase.firebaseio.com",
-    projectId: "oss-test-myfirebase",
-    storageBucket: "oss-test-myfirebase.appspot.com",
-    messagingSenderId: "10529373536"
+  apiKey: 'AIzaSyB9Trlbrpo48ilkNHZ6MGbofFf2u8uHuRA',
+  authDomain: 'oss-test-myfirebase.firebaseapp.com',
+  databaseURL: 'https://oss-test-myfirebase.firebaseio.com',
+  projectId: 'oss-test-myfirebase',
+  storageBucket: 'oss-test-myfirebase.appspot.com',
+  messagingSenderId: '10529373536'
 }
 
 Vue.use(VueFirestore)
@@ -22,20 +22,19 @@ var firestore = firebase.firestore()
 
 firestore.settings({timestampsInSnapshots: true})
 
-export function VueTick() {
+export function VueTick () {
   return new Promise((resolve, reject) => {
     Vue.nextTick(resolve)
   })
 }
 
 export function randomString () {
-    var string = '';
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var string = ''
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
-    for (var i = 0; i < 5; i++)
-    string += possible.charAt(Math.floor(Math.random() * possible.length));
+  for (var i = 0; i < 5; i++) { string += possible.charAt(Math.floor(Math.random() * possible.length)) }
 
-    return string;
+  return string
 }
 
 export { Vue }
