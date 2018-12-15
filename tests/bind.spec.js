affa73e48f620daf4f60b733ed3afd9ef40367db
Amrani Houssain <amranidev@gmail.com>
2018-12-15 12:38:01 +0000

    Add tests for docs

--- tests/bind.spec.js
+++ tests/bind.spec.js
@@ -1,28 +1,39 @@
 import { Vue, firebase, firestore, VueTick, randomString } from './TestCase'
 
-let vm, collection
+let vm, collection, doc
 let collectionName = randomString()
-
-describe('vue-firestore', () => {
-    beforeEach( async () => {
-      collection = firestore.collection('items')
-      vm = new Vue({
-        data: () => ({
+let documentName = randomString()
+describe('Manual binding', () => {
+  beforeEach(async () => {
+    collection = firestore.collection('items')
+    doc = firestore.doc('collectionOfDocs/doc')
+    vm = new Vue({
+      data: () => ({
             //
-        })
       })
-      await VueTick()
     })
+    await VueTick()
+  })
 
-    test('Bind collection manually', async () => {
-      await vm.$binding(collectionName, firestore.collection(collectionName))
-      expect(vm[collectionName]).toEqual([])
-      await vm.$firestore[collectionName].add({name: 'item'})
-      expect(vm[collectionName].length).toEqual(1)
-      expect(vm[collectionName][0].name).toEqual('item')
-    })
+  test('Bind collection manually', async () => {
+    await vm.$binding(collectionName, firestore.collection(collectionName))
+    expect(vm[collectionName]).toEqual([])
+    await vm.$firestore[collectionName].add({name: 'item'})
+    expect(vm[collectionName].length).toEqual(1)
+    expect(vm[collectionName][0].name).toEqual('item')
+  })
 
-    test('Binding collection returns promise', async () => {
-      expect(vm.$binding('someCollections', collection) instanceof Promise).toBe(true)
-    })
+  test('Bind document manually', async () => {
+    await vm.$binding(documentName, doc)
+    expect(vm.$firestore[documentName]).toBe(doc)
+    expect(vm[documentName]).toEqual({'.key': 'doc', 'name': 'docName'})
+  })
+
+  test('Binding collection returns promise', async () => {
+    expect(vm.$binding('someCollections', collection) instanceof Promise).toBe(true)
+  })
+
+  test('Binding document returns promise', async () => {
+    expect(vm.$binding('someCollections', doc) instanceof Promise).toBe(true)
+  })
 })
