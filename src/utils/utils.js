1b96b91d1cb62ed183b934b6889014060eae8782
Amrani Houssain <amranidev@gmail.com>
2019-02-23 14:35:00 +0100

    Wrap plugin default options into an object

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,16 +12,15 @@ export function isObject (val) {
  * Normalize Firebase snapshot into a bindable data record.
  *
  * @param {FirebaseSnapshot} snapshot
- * @param {string} keyName
- * @param {boolean} enumerable
+ * @param {object} options
  * @return {object}
  */
-export function normalize (snapshot, keyName = '.key', enumerable = false) {
+export function normalize (snapshot, options) {
   var value = snapshot.doc ? snapshot.doc.data() : snapshot.data()
   var out = isObject(value) ? value : { '.value': value }
-  Object.defineProperty(out, keyName, {
+  Object.defineProperty(out, options.keyName, {
     value: snapshot.doc ? snapshot.doc.id : snapshot.id,
-    enumerable: enumerable
+    enumerable: options.enumerable
   })
   return out
 }
